86c2a463ce161e1187ede96ebe15bfd840424ac4
Komarov Andrey <komarovandrey396@gmail.com>
2021-02-24 21:55:11 +0300

    added views and auth

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, redirect, url_for, render_template
+from website import create
 
-app = Flask(__name__, template_folder='templates')
+app = create()
 
 @app.route('/')
 def home():
     return (render_template('base.html'))
+
+#TODO Add custom error-handlers
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
