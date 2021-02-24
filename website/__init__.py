95eea0b77704072a52c4e417ac261d0f3dfded9f
Komarov Andrey <komarovandrey396@gmail.com>
2021-02-25 00:24:40 +0300

    added base for UI

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,12 @@ def create():
     app = Flask(__name__, template_folder='templates')
     app.config['SECRET_KEY'] = 'qkjkdgnsdfrjgakdsfmn'
 
+    from .views import views 
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+
+
     return app
